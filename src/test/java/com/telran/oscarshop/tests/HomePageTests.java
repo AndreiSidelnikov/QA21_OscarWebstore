b12ad66dcd9622b6423e63433b328941912c6e42
Andrei <sidelnikovandrey@gmail.com>
2021-12-21 13:00:34 +0100

    Add TestBase, HomePageTests

---
+++ src/test/java/com/telran/oscarshop/tests/HomePageTests.java
@@ -0,0 +1,15 @@
+package com.telran.oscarshop.tests;
+
+import com.telran.oscarshop.pages.HomePage;
+import com.telran.oscarshop.pages.ProductPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HomePageTests extends TestBase{
+
+    @Test
+    public void guestCanSelectProductFromMenuTest() {
+        new HomePage(driver).selectCategory();
+        Assert.assertTrue(new ProductPage(driver).isItProductPage());
+    }
+}
